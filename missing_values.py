7ba10747c466a586443011dd6e90995425f01678
Hariharan Jayachandran <hariharanjayachandran04@gmail.com>
2024-04-07 21:09:26 +0530

    missing value

--- missing_values.py
+++ missing_values.py
@@ -4,5 +4,9 @@ import pandas as pd
 df = pd.read_csv('walmart.csv')
 
 df.fillna(0, inplace=True)
+df.fillna(df.mean(), inplace=True)
+df.fillna(df.min(), inplace=True)
+df.fillna(df.max(), inplace=True)
+df.fillna("empty", inplace=True)
 
 df.to_csv('missing.csv', index=False)
